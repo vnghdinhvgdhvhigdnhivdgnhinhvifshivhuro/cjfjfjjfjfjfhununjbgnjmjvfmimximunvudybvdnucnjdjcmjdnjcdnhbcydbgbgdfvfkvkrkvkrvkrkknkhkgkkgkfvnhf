38934cb69b8d17ca4a9bdb229b7e5820fbe0fc5f
vnghdinhvgdhvhigdnhivdgnhinhvifshivhuro <ando250313@gmail.com>
2025-05-03 19:46:31 +1000

    Update m1 reset.lua

--- m1 reset.lua
+++ m1 reset.lua
@@ -20,13 +20,6 @@ UIS.InputBegan:Connect(function(input, gameproc)
         local remote = player.Character:FindFirstChild("Communicate")
         if remote and remote:IsA("RemoteEvent") then
             remote:FireServer(frontdash[1])
-            game.StarterGui:SetCore("SendNotification", {
-                Title = "Frontdash Activated",
-                Text = "You pressed R! Executing frontdash...",
-                Icon = "rbxassetid://5835996593",
-                Duration = 5,
-                Button1 = "Dismiss"
-            })
         else
             warn("❌ Communicate RemoteEvent not found!")
         end
@@ -45,23 +38,7 @@ local isWhitelisted = whitelistedNames[player.Name] or false
 
 if isWhitelisted then
     print("✅ You are whitelisted! 2 m1, turn press E, m1 again then sidedash and press R")
-    
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Thanks For Buying",
-        Text = "Thank You For Supporting Me",
-        Icon = "rbxassetid://5835996593",
-        Duration = 5,
-        Button1 = "Dismiss"
-    })
 else
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Access Denied",
-        Text = "❌ You don't have access to this script. Contact Androo_22 on Discord if this was a mistake.",
-        Icon = "rbxassetid://5835996593",
-        Duration = 5,
-        Button1 = "Dismiss"
-    })
-    
     wait(2)
     player:Kick("❌ You don't have access to this script. If this was a mistake, contact Androo_22 on Discord.")
 end
